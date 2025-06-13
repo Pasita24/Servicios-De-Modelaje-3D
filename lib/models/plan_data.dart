046ab387407b040a1eddfc74b7fc8460553b3509
Pasita <127953530+Pasita24@users.noreply.github.com>
2025-06-13 00:30:52 -0400

    Se modifico la clase plan_Data
    Se modifico la clase plan_Data para añadir datos como id, topmap y fromMap

--- lib/models/plan_data.dart
+++ lib/models/plan_data.dart
@@ -1,13 +1,35 @@
 class PlanData {
+  final int? id;
   final String category;
   final String title;
   final String imagePath;
   final String description;
 
   PlanData({
+    this.id,
     required this.category,
     required this.title,
     required this.imagePath,
     required this.description,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'category': category,
+      'title': title,
+      'imagePath': imagePath,
+      'description': description,
+    };
+  }
+
+  factory PlanData.fromMap(Map<String, dynamic> map) {
+    return PlanData(
+      id: map['id'] as int?,
+      category: map['category'] as String,
+      title: map['title'] as String,
+      imagePath: map['imagePath'] as String,
+      description: map['description'] as String,
+    );
+  }
 }
---
+++ lib/pages/database_helper.dart
@@ -0,0 +1,66 @@
+import 'package:sqflite/sqflite.dart';
+
+import 'package:path/path.dart';
+import 'package:servicios_de_modelaje3d/models/plan_data.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('plans.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+           CREATE TABLE plans (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             category TEXT,
+             title TEXT,
+             imagePath TEXT,
+             description TEXT
+           )
+         ''');
+  }
+
+  Future<int> createPlan(PlanData plan) async {
+    final db = await database;
+    return await db.insert('plans', plan.toMap());
+  }
+
+  Future<List<PlanData>> getPlans() async {
+    final db = await database;
+    final result = await db.query('plans');
+    return result.map((map) => PlanData.fromMap(map)).toList();
+  }
+
+  Future<int> updatePlan(PlanData plan) async {
+    final db = await database;
+    return await db.update(
+      'plans',
+      plan.toMap(),
+      where: 'id = ?',
+      whereArgs: [plan.id],
+    );
+  }
+
+  Future<int> deletePlan(int id) async {
+    final db = await database;
+    return await db.delete('plans', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<void> close() async {
+    final db = await database;
+    await db.close();
+  }
+}
